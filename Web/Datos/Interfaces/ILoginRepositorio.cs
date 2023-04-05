588fc5022bef47faf8c03d6a3f1a0edc00163a48
1Yamileth <Yamilethaguilarma@gmail.com>
2023-04-04 19:24:57 -0600

    Actualizacion
    Creacion de login y usuario en "Modelos:. Creacion de las carpetas "repositorio"  "interfaz". Empece con la clase de "LoginRepositorio" y el metodo de validar usuario. Min-9:39, Clase 29 de Marzo

---
+++ Web/Datos/Interfaces/ILoginRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelos;
+
+namespace Datos.Interfaces
+{//Solo desclaramos los metodos, nada de ingresar codigo
+    //Asi los podemos usar en las clases 
+    public interface ILoginRepositorio
+    {
+        Task<bool> ValidarUsuario(Login login);
+    }
+}
---
+++ Web/Datos/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using Datos.Interfaces;
+using Modelos;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorios
+{
+    public class LoginRepositorio : ILoginRepositorio/*Estamos heredadnod de la interfaz login repositorio*/
+    {/*Al momento de heredarlo, nos lanza de un solo los metodos declarados en esa interfaz, solo falta codificar ese metodo aca*/
+        private string CadenaConexion;//Recibira la cadena de conexion para conectarse a la BD.
+       
+        //COSNTRUCTOR
+        public LoginRepositorio (string cadenaconexion)//Le pasamos la cadena desde el proyecto blazor, por medio de este constructor
+        {
+            CadenaConexion = cadenaconexion;
+        }
+        //Declaramos la conexion hacia MYSQL
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);//Pasando la conexion a mysql
+        }
+
+
+        public Task<bool> ValidarUsuario(Login login)
+        {
+            bool valido = false;
+            try
+            {
+                using MySqlConnection _conexion = Conexion();
+            }
+            catch (Exception )
+            {
+
+            }
+        }
+    }
+}
